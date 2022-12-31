770e7adf59235c6b22e7d6298490b1ca7f6066ed
thewanionly <pelloani@gmail.com>
2022-12-31 16:02:21 +0800

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Elloani Ross Pitogo | Frontend Engineer</title>
+        <meta
+          name="description"
+          content="Portfolio site showcasing Elloani's projects"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>Test</main>
+    </>
+  )
+}
