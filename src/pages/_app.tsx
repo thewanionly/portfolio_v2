971b10bd74f2f9615902e8e82f1101d3bdba1810
thewanionly <pelloani@gmail.com>
2023-01-03 19:09:43 +0800

    Move `styles` into `common` folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app'
-import { GlobalStyles, Theme } from '../styles'
+import { GlobalStyles, Theme } from '../common/styles'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
