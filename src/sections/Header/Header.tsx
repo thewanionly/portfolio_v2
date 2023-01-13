b5f07e0c9fd65d336787d0b46a10052a29d081bf
Elloani Ross A. Pitogo <pelloani@gmail.com>
2023-01-13 20:53:22 +0800

    Add header logo (WIP) (#13)

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,16 +1,29 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import styled from 'styled-components'
+
+const S = {
+  HeaderLogoContainer: styled.div`
+    user-select: none;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+
+    margin: 0 auto;
+
+    height: 2.4rem;
+    position: relative;
+  `,
+  HeaderLogo: styled(Image)``,
+}
 
 const Header = () => {
   return (
     <header>
       <Link href="#">
-        <Image
-          src="/images/logo.svg"
-          alt="Header logo"
-          width={'100'}
-          height={'50'}
-        />
+        <S.HeaderLogoContainer>
+          <S.HeaderLogo src="/images/logo.svg" alt="Header logo" fill />
+        </S.HeaderLogoContainer>
       </Link>
     </header>
   )
