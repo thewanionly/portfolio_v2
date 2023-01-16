773d3c81a7d471442e328f77ff4f36cd710f95c5
thewanionly <pelloani@gmail.com>
2023-01-16 15:17:48 +0800

    Add `tests/setup.tsx`
    Add a custom render function which will wrap whatever component we pass into Theme provider

---
+++ src/common/tests/setup.tsx
@@ -0,0 +1,26 @@
+import { ReactElement } from 'react'
+import { render, RenderOptions, RenderResult } from '@testing-library/react'
+
+import { GlobalStyles, Theme } from '../styles'
+
+type RootWrapperProps = {
+  children?: React.ReactNode
+}
+
+const RootWrapper = ({ children }: RootWrapperProps) => {
+  return (
+    <Theme>
+      <GlobalStyles />
+      {children}
+    </Theme>
+  )
+}
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'queries'>
+): RenderResult => render(ui, { wrapper: RootWrapper, ...options })
+
+export * from '@testing-library/react'
+
+export { customRender as render }
--- src/sections/Header/Header.test.tsx
+++ src/sections/Header/Header.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '../../common/tests'
 import Header from './Header'
 
 const setup = () => {
