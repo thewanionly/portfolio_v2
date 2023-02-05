2729d6b81bdebf975e4ec02b036f5dd8bd192587
thewanionly <pelloani@gmail.com>
2023-02-06 04:23:34 +0800

    Refactor and style Home components

--- src/sections/Home/Home.tsx
+++ src/sections/Home/Home.tsx
@@ -1,6 +1,40 @@
 import Link from 'next/link'
-import styled from 'styled-components'
-import { container } from '../../common/styles/utilities'
+import styled, { css } from 'styled-components'
+import { container, highlightText } from '../../common/styles/utilities'
+
+// TODO: Refactor into another component
+const button = css`
+  display: block;
+  padding: 1.5rem 2.5rem;
+  max-width: max-content;
+
+  text-transform: uppercase;
+  font-size: ${({ theme: { fontSizes } }) => fontSizes.fontSizeSm};
+  font-weight: ${({ theme: { fontWeights } }) => fontWeights.fontWeightBold};
+  letter-spacing: 0.1rem;
+  line-height: 1.8rem;
+  text-align: center;
+
+  transition: all 0.2s;
+
+  // primary
+  background-color: ${({ theme: { colors } }) => colors.primary};
+  color: ${({ theme: { colors } }) => colors.btn};
+
+  &:hover {
+    background-color: ${({ theme: { colors } }) => colors.primaryLight};
+  }
+`
+
+const buttonPrimaryOutline = css`
+  background-color: transparent;
+  border: 0.1rem solid ${({ theme: { colors } }) => colors.primary};
+  color: ${({ theme: { colors } }) => colors.primary};
+
+  &:hover {
+    background-color: ${({ theme: { colors } }) => colors.primaryLightest};
+  }
+`
 
 const S = {
   Home: styled.section`
@@ -21,18 +55,59 @@ const S = {
     text-transform: uppercase;
   `,
   HomeTitleSecondary: styled.span`
+    display: block;
     letter-spacing: 0.5rem;
     font-size: ${({ theme: { fontSizes } }) => fontSizes.fontSizeSm};
-
     color: ${({ theme: { colors } }) => colors.bodyLighter};
   `,
   HomeTitlePrimary: styled.span`
-    margin-top: 1.5rem;
+    display: block;
     letter-spacing: 0.3rem;
+    font-size: inherit;
+    line-height: inherit;
+    font-weight: inherit;
   `,
   HomeSubtitle: styled.div``,
-  HomeSubtitleText: styled.p``,
-  HomeCTAButtonGroup: styled.div``,
+  HomeSubtitleText: styled.p`
+    color: ${({ theme: { colors } }) => colors.bodyLight};
+    line-height: 2.5rem;
+  `,
+  HomeSubtitleTextHighlighy: styled.strong`
+    ${highlightText}
+  `,
+  HomeCTAButtonGroup: styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 2rem;
+  `,
+  HomeCTAButton: styled(Link)`
+    ${button}
+
+    flex: 1;
+    flex-basis: 45%;
+    max-width: 100%;
+    min-width: 29rem;
+
+    @media only screen and ${({ theme: { breakPoints } }) =>
+        breakPoints.tabletPortrait} {
+      max-width: 35rem;
+    }
+  `,
+  HomeCTAButtonOutline: styled(Link)`
+    ${button}
+    ${buttonPrimaryOutline}
+
+    flex: 1;
+    flex-basis: 45%;
+    max-width: 100%;
+    min-width: 29rem;
+
+    @media only screen and ${({ theme: { breakPoints } }) =>
+        breakPoints.tabletPortrait} {
+      max-width: 35rem;
+    }
+  `,
   HomeSocialGroup: styled.ul`
     display: flex;
     align-items: flex-start;
@@ -42,6 +117,8 @@ const S = {
   `,
   HomeSocialGroupItem: styled.li``,
   HomeSocialLink: styled(Link)`
+    transition: all 0.2s;
+
     &:hover {
       color: ${({ theme: { colors } }) => colors.primary};
     }
@@ -61,19 +138,29 @@ export const Home = () => (
       </S.HomeTitle>
       <S.HomeSubtitle data-testid="home-subtitle">
         <S.HomeSubtitleText>
-          You can call me <strong>Wani</strong>.
+          You can call me{' '}
+          <S.HomeSubtitleTextHighlighy>Wani</S.HomeSubtitleTextHighlighy>.
         </S.HomeSubtitleText>
         <S.HomeSubtitleText>
-          School-taught <strong>Computer Engineer</strong>,
+          School-taught{' '}
+          <S.HomeSubtitleTextHighlighy>
+            Computer Engineer
+          </S.HomeSubtitleTextHighlighy>
+          ,
         </S.HomeSubtitleText>
         <S.HomeSubtitleText>
-          Industry-taught
-          <strong>Front-end Web Developer</strong>.
+          Industry-taught{' '}
+          <S.HomeSubtitleTextHighlighy>
+            Front-end Web Developer
+          </S.HomeSubtitleTextHighlighy>
+          .
         </S.HomeSubtitleText>
       </S.HomeSubtitle>
       <S.HomeCTAButtonGroup>
-        <Link href="#projects">View my work</Link>
-        <Link href="#projects">Contact me</Link>
+        <S.HomeCTAButton href="#projects">View my work</S.HomeCTAButton>
+        <S.HomeCTAButtonOutline href="#contact">
+          Contact me
+        </S.HomeCTAButtonOutline>
       </S.HomeCTAButtonGroup>
       <S.HomeSocialGroup>
         <S.HomeSocialGroupItem>
