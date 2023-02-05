ffd64dd3b80fb49a185aeeeab52a74cf757b48ad
Elloani Ross A. Pitogo <pelloani@gmail.com>
2023-02-06 02:07:45 +0800

    Pass all Home section tests (#38)
    Need to refactor next to follow design

--- src/sections/Home/Home.test.tsx
+++ src/sections/Home/Home.test.tsx
@@ -5,19 +5,100 @@ const setup = () => {
   render(<Home />)
 }
 
-beforeEach(() => {
-  setup()
-})
-
 describe('Home section', () => {
   describe('Layout', () => {
-    it('has header title', () => {
+    it('displays a self-introduction text displayed as header text', () => {
+      setup()
+
       expect(
         screen.getByRole('heading', {
-          name: 'Elloani Ross Pitogo',
           level: 1,
         })
       ).toBeInTheDocument()
+    })
+
+    it('displays more details about me displayed in short sentences', () => {
+      setup()
+
+      expect(screen.getByTestId('home-subtitle')).toBeInTheDocument()
+    })
+
+    it('displays View My Work CTA button link', () => {
+      setup()
+
+      expect(screen.getByRole('link', { name: /view my work/i }))
+    })
+
+    it('displays Contact Me CTA button link', () => {
+      setup()
+
+      expect(screen.getByRole('link', { name: /contact me/i }))
+    })
+
+    it('displays Gmail icon', () => {
+      setup()
+
+      expect(
+        screen.getByRole('link', { name: 'Gmail icon' })
+      ).toBeInTheDocument()
+    })
+
+    it('displays LinkedIn icon', () => {
+      setup()
+
+      expect(screen.getByRole('link', { name: 'LinkedIn' })).toBeInTheDocument()
+    })
+
+    it('displays GitHub icon', () => {
+      setup()
+
+      expect(screen.getByRole('link', { name: 'GitHub' })).toBeInTheDocument()
+    })
+  })
+
+  describe('Interactions', () => {
+    it(`contains a link to "#projects" in the View My Work CTA button link`, () => {
+      setup()
+
+      expect(
+        screen.getByRole('link', { name: /view my work/i })
+      ).toHaveAttribute('href', '#projects')
+    })
+
+    it(`contains a link to "#contact" in the Contact me CTA button link`, () => {
+      setup()
+
+      expect(screen.getByRole('link', { name: /contact me/i })).toHaveAttribute(
+        'href',
+        '#projects'
+      )
+    })
+
+    it(`contains a link to "mailto:pelloani@gmail.com" in the Gmail icon`, () => {
+      setup()
+
+      expect(screen.getByRole('link', { name: 'Gmail icon' })).toHaveAttribute(
+        'href',
+        'mailto:pelloani@gmail.com'
+      )
+    })
+
+    it(`contains a link to "https://www.linkedin.com/in/pitogoelloaniross/" in the LinkedIn icon`, () => {
+      setup()
+
+      expect(screen.getByRole('link', { name: 'LinkedIn' })).toHaveAttribute(
+        'href',
+        'https://www.linkedin.com/in/pitogoelloaniross/'
+      )
+    })
+
+    it(`contains a link to "https://github.com/thewanionly/" in the GitHub icon`, () => {
+      setup()
+
+      expect(screen.getByRole('link', { name: 'GitHub' })).toHaveAttribute(
+        'href',
+        'https://github.com/thewanionly/'
+      )
     })
   })
 })
--- src/sections/Home/Home.tsx
+++ src/sections/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import styled from 'styled-components'
 import { container } from '../../common/styles/utilities'
 
@@ -16,7 +17,7 @@ const S = {
     margin-top: 6rem;
     text-align: center;
   `,
-  HomeTitle: styled.div`
+  HomeTitle: styled.h1`
     text-transform: uppercase;
   `,
   HomeTitleSecondary: styled.span`
@@ -25,9 +26,29 @@ const S = {
 
     color: ${({ theme: { colors } }) => colors.bodyLighter};
   `,
-  HomeTitlePrimary: styled.h1`
+  HomeTitlePrimary: styled.span`
     margin-top: 1.5rem;
     letter-spacing: 0.3rem;
+  `,
+  HomeSubtitle: styled.div``,
+  HomeSubtitleText: styled.p``,
+  HomeCTAButtonGroup: styled.div``,
+  HomeSocialGroup: styled.ul`
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 2rem;
+    color: ${({ theme: { colors } }) => colors.icon};
+  `,
+  HomeSocialGroupItem: styled.li``,
+  HomeSocialLink: styled(Link)`
+    &:hover {
+      color: ${({ theme: { colors } }) => colors.primary};
+    }
+  `,
+  HomeSocialIcon: styled.svg`
+    width: 2.4rem;
+    height: 2.4rem;
   `,
 }
 
@@ -38,6 +59,57 @@ export const Home = () => (
         <S.HomeTitleSecondary>Hi there, I am</S.HomeTitleSecondary>
         <S.HomeTitlePrimary>Elloani Ross Pitogo</S.HomeTitlePrimary>
       </S.HomeTitle>
+      <S.HomeSubtitle data-testid="home-subtitle">
+        <S.HomeSubtitleText>
+          You can call me <strong>Wani</strong>.
+        </S.HomeSubtitleText>
+        <S.HomeSubtitleText>
+          School-taught <strong>Computer Engineer</strong>,
+        </S.HomeSubtitleText>
+        <S.HomeSubtitleText>
+          Industry-taught
+          <strong>Front-end Web Developer</strong>.
+        </S.HomeSubtitleText>
+      </S.HomeSubtitle>
+      <S.HomeCTAButtonGroup>
+        <Link href="#projects">View my work</Link>
+        <Link href="#projects">Contact me</Link>
+      </S.HomeCTAButtonGroup>
+      <S.HomeSocialGroup>
+        <S.HomeSocialGroupItem>
+          <S.HomeSocialLink
+            href="mailto:pelloani@gmail.com"
+            target="_blank"
+            title="Gmail"
+          >
+            <S.HomeSocialIcon aria-label="Gmail icon">
+              <use xlinkHref="/icons/gmail.svg#img"></use>
+            </S.HomeSocialIcon>
+          </S.HomeSocialLink>
+        </S.HomeSocialGroupItem>
+        <S.HomeSocialGroupItem>
+          <S.HomeSocialLink
+            href="https://www.linkedin.com/in/pitogoelloaniross/"
+            target="_blank"
+            title="LinkedIn"
+          >
+            <S.HomeSocialIcon>
+              <use xlinkHref="/icons/linkedin.svg#img"></use>
+            </S.HomeSocialIcon>
+          </S.HomeSocialLink>
+        </S.HomeSocialGroupItem>
+        <S.HomeSocialGroupItem>
+          <S.HomeSocialLink
+            href="https://github.com/thewanionly/"
+            target="_blank"
+            title="GitHub"
+          >
+            <S.HomeSocialIcon>
+              <use xlinkHref="/icons/github.svg#img"></use>
+            </S.HomeSocialIcon>
+          </S.HomeSocialLink>
+        </S.HomeSocialGroupItem>
+      </S.HomeSocialGroup>
     </S.HomeContainer>
   </S.Home>
 )
