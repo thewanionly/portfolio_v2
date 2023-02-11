723cb3ef7f0eb3ca572fa51c3f4a6a4b6ba82184
Elloani Ross A. Pitogo <pelloani@gmail.com>
2023-02-12 04:21:49 +0800

    Revert "Revert Netlify CMS setup (#48)" (#49)
    This reverts commit b77ec3f1565d01e791df856af5ae330f64345a18.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,17 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] },
+    })
+    return cfg
+  },
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
