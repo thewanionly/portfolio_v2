a0d328c1a3be0121414a1f7bf7eb913ae4726b17
thewanionly <pelloani@gmail.com>
2023-02-26 21:00:46 +0800

    Delete icons folder. Add more icons in Icon.tsx

--- src/common/components/Icon/Icon.tsx
+++ src/common/components/Icon/Icon.tsx
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
+import { SlArrowLeft, SlArrowRight } from 'react-icons/sl'
 import { SiGithub, SiGmail, SiLinkedin } from 'react-icons/si'
 
 const IconMap = {
+  arrowLeft: SlArrowLeft,
+  arrowRight: SlArrowRight,
   github: SiGithub,
   gmail: SiGmail,
   linkedin: SiLinkedin,
 } as const
 
 export enum IconName {
+  ARROW_LEFT = 'arrowLeft',
+  ARROW_RIGHT = 'arrowRight',
   GITHUB = 'github',
   GMAIL = 'gmail',
   LINKEDIN = 'linkedin',
