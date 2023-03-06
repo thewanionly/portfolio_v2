98c32a5a3159436c29e9091ca240924ab3741713
thewanionly <pelloani@gmail.com>
2023-03-07 05:24:16 +0800

    Use Formik for ContactForm

--- src/sections/Contact/ContactForm/ContactForm.tsx
+++ src/sections/Contact/ContactForm/ContactForm.tsx
@@ -1,64 +1,65 @@
-import { ChangeEvent, ReactElement, useState } from 'react'
+import { ReactElement } from 'react'
+import styled from 'styled-components'
+import { Formik, Form, Field, FormikHelpers } from 'formik'
 
 import { useContentContext } from 'common/context'
+
+const S = {
+  ContactForm: styled.div`
+    color: ${({ theme: { colors } }) => colors.bodyLighter};
+  `,
+}
+
+interface ContactFormValues {
+  name: string
+  email: string
+  subject: string
+  message: string
+}
+
+const initialValues: ContactFormValues = {
+  name: '',
+  email: '',
+  subject: '',
+  message: '',
+}
 
 export const ContactForm = (): ReactElement => {
   const {
     contact: { submitBtnLabel },
   } = useContentContext()
 
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [subject, setSubject] = useState('')
-  const [message, setMessage] = useState('')
+  const handleSubmit = (
+    values: ContactFormValues,
+    { setSubmitting }: FormikHelpers<ContactFormValues>
+  ) => {
+    console.log(values)
+    setSubmitting(false)
+  }
 
   return (
-    <form>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          name="name"
-          value={name}
-          onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            setName(event.target.value)
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          name="email"
-          value={email}
-          onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            setEmail(event.target.value)
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="subject">Subject</label>
-        <input
-          id="subject"
-          name="subject"
-          value={subject}
-          onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            setSubject(event.target.value)
-          }
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          value={message}
-          onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
-            setMessage(event.target.value)
-          }
-        />
-      </div>
-      <button type="submit">{submitBtnLabel}</button>
-    </form>
+    <S.ContactForm>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <Field id="name" name="name" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <Field id="email" name="email" type="email" />
+          </div>
+          <div>
+            <label htmlFor="subject">Subject</label>
+            <Field id="subject" name="subject" />
+          </div>
+          <div>
+            <label htmlFor="message">Message</label>
+            <Field id="message" name="message" as="textarea" />
+          </div>
+          <button type="submit">{submitBtnLabel}</button>
+        </Form>
+      </Formik>
+    </S.ContactForm>
   )
 }
