f59bab239e8e1b3f5612369674980312c11e349f
thewanionly <pelloani@gmail.com>
2023-04-08 09:02:03 +0800

    Add Manrope font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,7 +32,18 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
