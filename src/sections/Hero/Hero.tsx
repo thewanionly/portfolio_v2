490f6a6ca0be5e0e4d074fb9b5094ed079c10230
thewanionly <pelloani@gmail.com>
2024-03-18 05:00:32 +0800

    feat: add my image in hero section

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,10 +1,22 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 import { Button, ButtonVariant } from 'common/components'
 import { SocialIcons } from 'common/components/SocialIcons'
 import { useContentContext } from 'common/context'
 import { container, highlightText } from 'common/styles/utilities'
 import { renderDescription } from 'common/utilities'
+
+// Credits to the ff for the fancy border radius animation:
+// - https://9elements.github.io/fancy-border-radius/#53.24.54.80--.
+// - https://9elements.com/blog/css-border-radius-can-do-that/
+const morph = keyframes`
+  0% {border-radius: 40% 60% 60% 40% / 60% 30% 70% 40%;}
+  100% {border-radius: 40% 60%;}
+`
+
+const spin = keyframes`
+ to { transform: rotate(1turn); }
+`
 
 const S = {
   Hero: styled.section`
@@ -20,9 +32,50 @@ const S = {
     gap: 3rem;
     margin-top: 6rem;
     text-align: center;
+    align-items: center;
   `,
   HeroTitle: styled.h1`
     text-transform: uppercase;
+  `,
+  HeroImageSpinContainer: styled.div`
+    margin: 3rem 0 2rem;
+    width: 20rem;
+    aspect-ratio: 1/1;
+    position: relative;
+    animation: ${spin} 12s ease-in-out infinite alternate;
+
+    @media only screen and ${({ theme: { breakPoints } }) =>
+        breakPoints.tabletPortrait} {
+      width: 25rem;
+      margin: 4rem 0 3rem;
+    }
+
+    @media only screen and ${({ theme: { breakPoints } }) =>
+        breakPoints.desktop} {
+      width: 30rem;
+    }
+  `,
+  HeroImageContainer: styled.div`
+    width: 100%;
+    height: 100%;
+    transition: border-radius 1s ease-out;
+    border-radius: 60% 40% 30% 70% / 60% 30% 70% 40%;
+    position: absolute;
+    overflow: hidden;
+  `,
+  HeroImage: styled.div`
+    width: 142%;
+    height: 142%;
+    position: absolute;
+    left: -21%;
+    top: -21%;
+
+    background-image: url('/img/me.jpg');
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+
+    animation: ${spin} 12s ease-in-out infinite alternate-reverse;
   `,
   HeroTitleSecondary: styled.span`
     display: block;
@@ -91,6 +144,11 @@ export const Hero = () => {
           <S.HeroTitleSecondary>{greeting} I am</S.HeroTitleSecondary>
           <S.HeroTitlePrimary>{fullName}</S.HeroTitlePrimary>
         </S.HeroTitle>
+        <S.HeroImageSpinContainer>
+          <S.HeroImageContainer>
+            <S.HeroImage />
+          </S.HeroImageContainer>
+        </S.HeroImageSpinContainer>
         <S.HeroSubtitle>
           <S.HeroNicknameText>
             {`${nicknameText.nicknameIntro} `}
